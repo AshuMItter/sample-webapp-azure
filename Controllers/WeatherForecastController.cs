5b258ceceb14d1d073c202ad5c0f814d8ad0e947
Ashu <ashu976sincere@gmail.com>
2026-05-05 02:12:21 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using sample_webapp_azure.Models;
+
+namespace sample_webapp_azure.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //private static readonly string[] Summaries =
+        //[
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //];
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<Demographics> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Demographics
+            {
+                PersonId = index,
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Name = $"SomeName{index.ToString()}",
+                Address = $"SomeAddress{index.ToString()}",
+                AndSoOn = $"SomeValue{index.ToString()}"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Models/Demographics.cs
@@ -0,0 +1,17 @@
+﻿namespace sample_webapp_azure.Models
+{
+    public class Demographics
+    {
+        public int PersonId { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Address { get; set; }
+
+
+        public DateOnly DateOfBirth { get; set; }
+
+
+        public string AndSoOn { get; set; }
+    }
+}
